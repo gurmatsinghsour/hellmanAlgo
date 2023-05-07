c1a6d511913653bb459de15e755f40a8798a0658
Gurmat Singh Sour <89722217+gurmatsinghsour@users.noreply.github.com>
2023-05-07 17:23:46 +0530

    Updated hellmanAlgo.py

--- hellmanAlgo.py
+++ hellmanAlgo.py
@@ -1,35 +1,51 @@
 import random
 
+# Function to check if a number is prime
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
 try:
-    p = int(input("Enter the value of sender : "))
-    q = int(input("Enter the value of receiver : "))
+    # Get prime numbers from the user
+    p = int(input("Enter a prime number: "))
+    if not is_prime(p):
+        raise ValueError("Value entered is not a prime number.")
+    q = int(input("Enter another prime number: "))
+    if not is_prime(q):
+        raise ValueError("Value entered is not a prime number.")
 except ValueError as err:
     print(err)
-    
+    exit()
 
+# Generate two secret large random numbers
+secret_key_a = random.randint(0, 1000)
+secret_key_b = random.randint(0, 1000)
 
-# 2 secret large random number
+# Make sure the secret keys are not equal
+if secret_key_a == secret_key_b:
+    secret_key_b = random.randint(0, 1000)
 
-a = random.randint(0,1000)
-b = random.randint(0,1000)
+# Calculate R and S
+R = (q ** secret_key_a) % p
+S = (q ** secret_key_b) % p
 
-if a == b:
-    b = random.randint(0,1000)
+# Calculate the secret keys of sender and receiver
+secret_key_sender = (S ** secret_key_a) % p
+secret_key_receiver = (R ** secret_key_b) % p
 
+# Print the results
+print("\n\n*** Value of prime numbers ***\n")
+print(f"Value of p is {p} and Value of q is {q} ")
 
-r = (q**a)%p
-s = (q**b)%p
+print("\n\n*** Secret large random numbers ***\n")
+print(f"Secret key of sender: {secret_key_a} Secret key of receiver: {secret_key_b}")
 
-r1 = (s**a)%p
-s1 = (r**b)%p
+print("\n\n*** Value of R and S (keys) ***\n")
+print(f"R -> q^a mod p = {q} ^ {secret_key_a} mod {p} = {R}\nS -> q^b mod p = {q} ^ {secret_key_b} mod {p} = {S}")
 
-print("\n\n***** Value of large prime numbers *****\n")
-print(f'Value of sender is {p} and Value of receiver is {q} ')
-print("\n\n***** Secret large random number *****\n")
-print(f'Value1 : {a} Value2 : {b}')
-print("\n\n***** Value of sender & receiver (key) *****\n")
-print(f'R -> q^a mod p = {q} ^ {a} mod {p} = {r}     &     S -> q^b mod p = {q} ^ {b} mod {p} = {s}')
-print("\n\n***** Secret key of sender & receiver *****\n")
-print(f'Rk -> s^a mod p = {s} ^ {a} mod {p} = {r1}     &     Sk -> r^b mod p = {r} ^ {b} mod {p} = {s1}')
-
-
+print("\n\n*** Secret key of sender and receiver ***\n")
+print(f"Secret key of sender -> S^a mod p = {S} ^ {secret_key_a} mod {p} = {secret_key_sender}\nSecret key of receiver -> R^b mod p = {R} ^ {secret_key_b} mod {p} = {secret_key_receiver}")
